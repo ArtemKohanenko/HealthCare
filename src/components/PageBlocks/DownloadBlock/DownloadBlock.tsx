ca1aca61ca0a84cb60f2c14013e0e5f8f6c83927
ArtemKohanenko <kartem0255@gmail.com>
2023-12-15 18:11:49 +0600

    убрал relativeContainer из DownloadBlock

--- src/components/PageBlocks/DownloadBlock/DownloadBlock.tsx
+++ src/components/PageBlocks/DownloadBlock/DownloadBlock.tsx
@@ -5,19 +5,16 @@ import classes from './DownloadBlock.module.scss'
 
 const DownloadBlock = () => {
     return <>
-        <div className={classes.relativeContainer}>
-            <img className={classes.fixedPoints} src='src/assets/points_background.svg'></img>
-            <div className={classes.container}>
-                <div className={classes.info}>
-                    <div className={classes.textBlock}>
-                        <span className={classes.title}>Download our mobile apps</span>
-                        <Divider/>
-                        <span className={classes.text}>Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely</span>
-                    </div>
-                    <BlankButton text='Download' icon_path='src/assets/download.svg'/>
+        <div className={classes.container}>
+            <div className={classes.info}>
+                <div className={classes.textBlock}>
+                    <span className={classes.title}>Download our mobile apps</span>
+                    <Divider/>
+                    <span className={classes.text}>Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely</span>
                 </div>
-                <img className={classes.picture} src='src/assets/downloadApp_block_picture.png'/>
+                <BlankButton text='Download' icon_path='src/assets/download.svg'/>
             </div>
+            <img className={classes.picture} src='src/assets/downloadApp_block_picture.png'/>
         </div>
     </>
 }
