56ba8eebdef8e584e0645c1448b5f17cf7f7b5a5
ArtemKohanenko <kartem0255@gmail.com>
2023-12-16 12:56:35 +0600

    использовал тег nav в navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,13 +3,13 @@ import classes from './NavBar.module.scss'
 
 const NavBar = () => {
     return <>
-        <div className={classes.container}>
+        <nav className={classes.container}>
             <span className={classes.item}>Home</span>
             <span className={classes.item}>Find a doctor</span>
             <span className={classes.item}>Apps</span>
             <span className={classes.item}>Testimonials</span>
             <span className={classes.item}>About us</span>
-        </div>
+        </nav>
     </>
 }
 
