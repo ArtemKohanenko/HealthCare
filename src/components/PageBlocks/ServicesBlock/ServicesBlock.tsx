5850828b0712c29e4aed5ba7d840cd10901612c7
ArtemKohanenko <kartem0255@gmail.com>
2023-12-23 21:18:59 +0600

    рефакторинг

--- src/components/PageBlocks/ServicesBlock/ServicesBlock.tsx
+++ src/components/PageBlocks/ServicesBlock/ServicesBlock.tsx
@@ -5,14 +5,6 @@ import Divider from '../../Divider/Divider';
 
 
 const ServicesBlock = () => {
-    const services = [
-        {img_name: 'src/assets/service_picture_1.svg', title: 'Search doctor', text: 'Choose your doctor from thousands of specialist, general, and trusted hospitals'},
-        {img_name: 'src/assets/service_picture_2.svg', title: 'Online pharmacy', text: 'Buy  your medicines with our mobile application with a simple delivery system'},
-        {img_name: 'src/assets/service_picture_3.svg', title: 'Consultation', text: 'Free consultation with our trusted doctors and get the best recomendations'},
-        {img_name: 'src/assets/service_picture_4.svg', title: 'Details info', text: 'Free consultation with our trusted doctors and get the best recomendations'},
-        {img_name: 'src/assets/service_picture_5.svg', title: 'Emergency care', text: 'You can get 24/7 urgent care for yourself or your children and your lovely family'},
-        {img_name: 'src/assets/service_picture_6.svg', title: 'Tracking', text: 'Track and save your medical history and health data '}
-    ]
 
     return <>
         <section className={classes.container}>
