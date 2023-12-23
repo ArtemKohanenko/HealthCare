6e1b840683e1ca9356f77e326da90d67b7a81a4b
ArtemKohanenko <kartem0255@gmail.com>
2023-12-23 12:37:56 +0600

    добавил hover эффекты на кнопки и ссылки

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -3,16 +3,14 @@ import classes from './Link.module.scss'
 
 const Link = (props: {text: string, icon_path?: string}) => {
     return <>
-        <button className={classes.button}>
-            <div className={classes.container}>
-                <span className={classes.text}>{props.text}</span>
-                {
-                    props.icon_path
-                    ? <img src={props.icon_path} className={classes.icon}></img>
-                    : null
-                }
-            </div>
-        </button>
+        <div className={classes.container}>
+            <a className={classes.text}>{props.text}</a>
+            {
+                props.icon_path
+                ? <img src={props.icon_path} className={classes.icon}></img>
+                : null
+            }
+        </div>
     </>
 }
 
